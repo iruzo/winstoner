6d37f6ca5cf64d764c61efad3a56eefcfdea3354
iruzo <hidden>
2022-12-07 23:51:43 +0100

    init

---
+++ winstoner.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (int argc, char **argv)
+{
+  for (int i = 1; i < argc; i++) {
+    printf("%s%s\n", argv[i], "ston");
+  }
+  return 0;
+}
